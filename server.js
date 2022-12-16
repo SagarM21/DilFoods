b8e4212679d8e0d31d03ad47f89f687a18e2a2ce
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-16 15:06:14 +0530

    getting [] 😒

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const db = require("./db");
+const thali = require("./models/thaliModel");
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Server Working🔥");
+});
+
+app.get("/getThalis", (req, res) => {
+	thali.find({}, (err, docs) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(docs);
+		}
+	});
 });
 
 const port = process.env.PORT || 5000;
