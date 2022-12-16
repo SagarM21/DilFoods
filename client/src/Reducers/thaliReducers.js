2434b629d25db8a3c1913efd4e4a42c94adf2bec
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-16 16:45:56 +0530

    loading added

--- client/src/Reducers/thaliReducers.js
+++ client/src/Reducers/thaliReducers.js
@@ -1,15 +1,18 @@
-export const getAllThalisReducer = (state = {}, action) => {
+export const getAllThalisReducer = (state = { thalis: [] }, action) => {
 	switch (action.type) {
 		case "GET_THALI_REQUEST":
 			return {
+				loading: true,
 				...state,
 			};
 		case "GET_THALI_SUCCESS":
 			return {
+				loading: false,
 				thalis: action.payload,
 			};
 		case "GET_THALI_FAILED":
 			return {
+				loading: false,
 				error: action.payload,
 			};
 		default:
