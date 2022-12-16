7e16e565ee8773b3af7a5c860af7eb0827cbd93d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-16 19:52:20 +0530

    register user -- frontend

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,12 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { getAllThalisReducer } from "./Reducers/thaliReducers";
 import { cartReducer } from "./Reducers/cartReducers";
+import { registerUserReducer } from "./Reducers/userReducers";
 
 const finalReducer = combineReducers({
 	getAllThalisReducer: getAllThalisReducer,
 	cartReducer: cartReducer,
+	registerUserReducer: registerUserReducer,
 });
 
 const cartItems = localStorage.getItem("cartItems")
