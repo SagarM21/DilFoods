86350aaab9e10b8c2c3881293e82ed4432f81084
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-16 16:50:23 +0530

    made dynamic, fetching data from backend, saved to redux and displayed

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -2,25 +2,32 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllThalis } from "../Actions/thaliActions";
 import Thali from "../components/Thali";
-import thalis from "../data";
 
 const Home = () => {
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
+	const thaliState = useSelector((state) => state.getAllThalisReducer);
+	const { thalis, error, loading } = thaliState;
 	useEffect(() => {
-		dispatch(getAllThalis())
+		dispatch(getAllThalis());
 	}, []);
 	return (
 		<div>
 			<div className='row'>
-				{thalis.map((thali, i) => {
-					return (
-						<div className='col-md-6' key={i}>
-							<div>
-								<Thali thali={thali} />
+				{loading ? (
+					<h1>Loading...</h1>
+				) : error ? (
+					<h1>Something went wrong</h1>
+				) : (
+					thalis.map((thali, i) => {
+						return (
+							<div className='col-md-6' key={i}>
+								<div>
+									<Thali thali={thali} />
+								</div>
 							</div>
-						</div>
-					);
-				})}
+						);
+					})
+				)}
 			</div>
 		</div>
 	);
